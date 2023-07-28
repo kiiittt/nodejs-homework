3760f50307788fb177b6a1cb3819cd448bfac00c
Darya <daryakolisnik@gmail.com>
2023-07-28 20:30:04 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const yargs = require("yargs");
+// index.js
+const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const {
   listContacts,
@@ -8,62 +9,31 @@ const {
 } = require("./contacts");
 
 const argv = yargs(hideBin(process.argv))
-  .usage("Usage: $0 <command> [options]")
-  .command("list", "List all contacts")
-  .command("get <id>", "Get contact by id", (yargs) => {
-    yargs.positional("id", {
-      describe: "Contact id",
-      type: "string",
-    });
-  })
-  .command("add", "Add a new contact", (yargs) => {
-    yargs.option("name", {
-      alias: "n",
-      describe: "Contact name",
-      demandOption: true,
-      type: "string",
-    });
-    yargs.option("email", {
-      alias: "e",
-      describe: "Contact email",
-      demandOption: true,
-      type: "string",
-    });
-    yargs.option("phone", {
-      alias: "p",
-      describe: "Contact phone",
-      demandOption: true,
-      type: "string",
-    });
-  })
-  .command("remove <id>", "Remove contact by id", (yargs) => {
-    yargs.positional("id", {
-      describe: "Contact id",
-      type: "string",
-    });
-  })
-  .demandCommand(1, "You need at least one command before moving on")
-  .strict()
-  .help()
-  .alias("h", "help").argv;
-
-const { _, $0, ...args } = argv;
-
-function invokeAction(action, args) {
-  switch (action) {
-    case "list":
+  .command({
+    command: "list",
+    describe: "List all contacts",
+    handler: () => {
       listContacts()
         .then((contacts) => {
           console.log("Список контактів:");
-          console.log(contacts);
+          console.table(contacts);
         })
         .catch((error) => {
           console.error("Помилка при отриманні списку контактів:", error);
         });
-      break;
-
-    case "get":
-      getContactById(args.id)
+    },
+  })
+  .command({
+    command: "get <id>",
+    describe: "Get contact by id",
+    builder: (yargs) => {
+      return yargs.positional("id", {
+        describe: "Contact id",
+        type: "string",
+      });
+    },
+    handler: (argv) => {
+      getContactById(argv.id)
         .then((contact) => {
           if (contact) {
             console.log("Знайдений контакт:");
@@ -75,10 +45,34 @@ function invokeAction(action, args) {
         .catch((error) => {
           console.error("Помилка при отриманні контакту за id:", error);
         });
-      break;
-
-    case "add":
-      addContact(args.name, args.email, args.phone)
+    },
+  })
+  .command({
+    command: "add",
+    describe: "Add a new contact",
+    builder: (yargs) => {
+      return yargs
+        .option("name", {
+          alias: "n",
+          describe: "Contact name",
+          demandOption: true,
+          type: "string",
+        })
+        .option("email", {
+          alias: "e",
+          describe: "Contact email",
+          demandOption: true,
+          type: "string",
+        })
+        .option("phone", {
+          alias: "p",
+          describe: "Contact phone",
+          demandOption: true,
+          type: "string",
+        });
+    },
+    handler: (argv) => {
+      addContact(argv.name, argv.email, argv.phone)
         .then((addedContact) => {
           console.log("Доданий новий контакт:");
           console.log(addedContact);
@@ -86,10 +80,19 @@ function invokeAction(action, args) {
         .catch((error) => {
           console.error("Помилка при додаванні нового контакту:", error);
         });
-      break;
-
-    case "remove":
-      removeContact(args.id)
+    },
+  })
+  .command({
+    command: "remove <id>",
+    describe: "Remove contact by id",
+    builder: (yargs) => {
+      return yargs.positional("id", {
+        describe: "Contact id",
+        type: "string",
+      });
+    },
+    handler: (argv) => {
+      removeContact(argv.id)
         .then((removedContact) => {
           if (removedContact) {
             console.log("Видалений контакт:");
@@ -101,12 +104,9 @@ function invokeAction(action, args) {
         .catch((error) => {
           console.error("Помилка при видаленні контакту за id:", error);
         });
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-}
-
-const action = _.shift();
-invokeAction(action, args);
+    },
+  })
+  .demandCommand(1, "You need at least one command before moving on")
+  .strict()
+  .help()
+  .alias("h", "help").argv;
